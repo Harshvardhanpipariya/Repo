cb785fb1ebfb1b42f935617c40036d26171da6a2
harshvardhanpipariya <harshvardhanpipariya65@gmail.com>
2026-04-13 20:19:12 +0530

    added type of device in model

--- v2_Models/Device.js
+++ v2_Models/Device.js
@@ -5,6 +5,12 @@ const deviceSchema = new mongoose.Schema(
   {
     device_name: { type: String, required: true, index: true },
     installation_date: Date,
+     device_type: {
+      type: String,
+      enum: ["lora_box", "kache_box", "TypeC"], // allowed values
+      required: true,
+      index: true,
+    },
     software_version: String,
     region_id: {
       type: mongoose.Schema.Types.ObjectId,
